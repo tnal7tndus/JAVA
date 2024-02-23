48afff6e3ac3734bf7b5295eda600cff1a0853d0
'ksm' <'tnal7tndus@naver.com'>
2024-02-23 18:03:41 +0900

    2024.02.23

--- demoM/src/main/webapp/resources/myLib/axTest03.js
+++ demoM/src/main/webapp/resources/myLib/axTest03.js
@@ -12,19 +12,7 @@
 // => response를 reasultArea1에 출력하기
 // => 요청명: /member/aximlist
 // => response: axmemberList.jsp
-function axiMListf(){
-	let resultHtml =
-	`<table align center>
-		<caption><h2>** Web02_MVC02 MemberList **</h2></cpation>
-		<tr height=50><td bgcolor="yellow"><label for="id">ID</label></td>
-			<td><input type="text" name="id" id="id"></td>
-		</tr>
-		<tr height=50><td bgcolor="yellow"><label for="name">Name</label></td>
-			<td><input type="name" name="name" id="name"></td>
-		</tr>
-	</table>`
-	
-	document.getElementById('resultArea1').innerHTML=resultHtml
+
 	
 function axiMList(){
 		
@@ -33,7 +21,7 @@ function axiMList(){
 	axios.get(
 		url).then(response => {
 			console.log("** response 성공 **");
-			document.getElementById('resultArea1').innerHTML=response;
+			document.getElementById('resultArea1').innerHTML=response.data;
 	}).catch(err => {
 			alert("** response 실패 => " + err.message);
 	});
